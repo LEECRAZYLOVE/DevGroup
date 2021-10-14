c809a897625e13fba209b875fc8bf22465155d71
MertzCurtz <mcnotrem98@gmail.com>
2021-10-14 15:43:15 +0200

    HomeAdmin/Gen

---
+++ HomeAdmin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Itim">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="CSS\Landing.css">   
+</head>
+<body>
+<!--Start of the global navigation section-->
+<header>
+  <nav>
+    <!--Start of header_labels-->
+    <div class = "global_nav">
+    <ul class = "header_labels"> <!--Might have to change this class name when we get to CSS-->
+      <li style="float:center"> <img id="logo" src="images/TCGplayer-logo.png" alt="Store logo" style="margin-left: 600px; width:140px; height:80px;"> </li>
+      <li style="float:right"> <a href="UserProfile.html"> <img src="images/user-icon.png" alt="User Profile" style="width:50px; height:50px;"></a> </li>
+    </ul>
+    </div>
+    <!--End header_labels--> 
+  </nav>
+</header>
+<!--End of the global navigation section-->
+
+<main>
+</main>
+
+<!--Just a footer for profesionalism-->
+<footer>
+    <p>&copy; 2021 NameOfStore.&nbsp;&nbsp; All Rights Reserved. &nbsp;&nbsp;Proudly created by
+        NameOfStore
+    </p>
+    <p>
+        <small>Last modified:
+            <time>14 October 2021</time>
+        </small>
+    </p>
+</footer>
+</body>
+</html>

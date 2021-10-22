a76219cbf988d2891b6429332e335c12c65d76a3
wvdm99 <dwilhelmvdm@gmail.com>
2021-10-22 07:50:23 +0200

    Sell request update

--- Sell_Request_Query.php
+++ Sell_Request_Query.php
@@ -1,6 +1,7 @@
 <?php
 
 //request id auto increments
+$setname = $_REQUEST['setname'];
 $cardname = $_REQUEST['card_name'];
 $category = $_REQUEST['category'];
 $adminid = "mert9312";
@@ -8,7 +9,6 @@ $sellerid = 0;
 $status = NULL;
 $quality = $_REQUEST["quality"];
 
-$setname = $_REQUEST['setname'];
 $cardtype = $_REQUEST['cardtype'];
 $rarity = $_REQUEST['rarity'];
 $colour = $_REQUEST['colour'];
@@ -23,6 +23,7 @@ $class = $_REQUEST['class'];
 $age = $_REQUEST['age'];
 $description = $_REQUEST['description'];
 $price = $_REQUEST['price'];
+$picture = $_REQUEST['image'];
 
 //Image Upload
 $target_dir = "Images/Cards/";
@@ -38,8 +39,11 @@ require_once("config.php");
 $connect = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
                 or die("<strong style = \"color : red; \"> Could not connect to the database! </strong>");
 
-$query =  "INSERT INTO sell_request (Admin_ID, Card_Name, Category, Set_Name, Card_Type, Rarity, Colour, Nation, Grade, Skill_Icon, Character, Era, Sub_Type, Class, Age, Description, Image, Price, Seller_ID, Quality)
-          VALUES ('$adminid', '$cardname', '$category','$setname', '$cardtype', '$rarity', '$colour', '$nation', '$grade', '$skillicon', '$dbzcharacter', '$era', '$subtype', '$class', '$age', '$description', '$image', '$price', '$sellerid', '$quality')";
+// $query =  "INSERT INTO sell_request (Admin_ID, Card_Name, Category, Set_Name, Card_Type, Rarity, Colour, Nation, Grade, Skill_Icon, Character, Era, Sub_Type, Class, Age, Description, Image, Price, Seller_ID, Quality)
+//           VALUES ('$adminid', '$cardname', '$category','$setname', '$cardtype', '$rarity', '$colour', '$nation', '$grade', '$skillicon', '$dbzcharacter', '$era', '$subtype', '$class', '$age', '$description', '$image', '$price', '$sellerid', '$quality')";
+
+$query =  "INSERT INTO sell_request (Admin_ID, Card_Name, Category, Age, Description, Image, Price, Seller_ID, Quality, Set_Name)
+VALUES ('$adminid', '$cardname', '$category', '$age', '$description', '$picture', '$price', '$sellerid', '$quality', '$setname')";
 
 $result = mysqli_query($connect, $query)
                 or die("<strong style = \"color : red; \"> Could not execute query! </strong>");  
